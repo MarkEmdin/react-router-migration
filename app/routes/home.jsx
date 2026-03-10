c3c839df7bb15b39448b85492e35e96c1f28d760
Mark Emdin <mark@vibez.io>
2026-03-10 11:59:35 +0200

    added all sections

--- app/routes/home.jsx
+++ app/routes/home.jsx
@@ -1,11 +1,19 @@
 import { ThemeProvider } from '@rescui/ui-contexts';
 import { UsageSection } from '../sections/usageSection';
+import { HeaderSection } from '../sections/headerSection';
+import { StartSection } from '../sections/startSection';
+import { LatestFromKotlinSection } from '../sections/latestFromKotlinSection';
+import { WhyKotlinSection } from '../sections/whyKotlinSection';
 
 export default function Home() {
   return (
     <ThemeProvider theme="dark">
       <div>
+        <HeaderSection />
+        <LatestFromKotlinSection />
+        <WhyKotlinSection />
         <UsageSection />
+        <StartSection />
       </div>
     </ThemeProvider>
   );
